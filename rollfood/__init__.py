f75727c2132566a8be45ddbbbcbbfe83b01ad91c
Brad Wenner <brad@bradwenner.com>
2026-01-06 21:23:39 -0800

    [rollfood] settings, optional AI prompt

--- rollfood/__init__.py
+++ rollfood/__init__.py
@@ -7,10 +7,6 @@ from redbot.core.bot import Red
 
 from .rollfood import RollFood
 
-with Path(__file__).parent.joinpath("info.json").open() as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-
 async def setup(bot: Red) -> None:
     """Load RollFood cog."""
     cog = RollFood(bot)
